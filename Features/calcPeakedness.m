888beff6bb8e8f6b7c625e525025c0562367f961
rqalbuquerque <rqa@cin.ufpe.br>
2017-06-11 01:40:02 -0300

    Alterado - Separação dos arquivos em pastas. Inclusão - Scripts de treinamento do classificador Bayesiano.

---
+++ Features/calcPeakedness.m
@@ -0,0 +1,31 @@
+function q = calcPeakedness(img, pSize)
+
+    [im_height, im_width] = size(img);
+    offset = (pSize - 1)/2;
+
+    % gradiente
+    [dx, dy] = gradient(uint8(img));
+
+    % patches e normalização utilizada no projeto original
+    dx_col = im2col(dx, [pSize, pSize]);
+    dx_col = bsxfun(@rdivide, dx_col, sum(dx_col));
+    dy_col = im2col(dy, [pSize, pSize]);
+    dy_col = bsxfun(@rdivide, dy_col, sum(dy_col));
+
+    % Kurtosis
+    qx = moment(dx_col,4)./(moment(dx_col,2).^2);
+    qy = moment(dy_col,4)./(moment(dy_col,2).^2);
+
+    qx = reshape(qx, [im_height-pSize+1, im_width-pSize+1]);
+    qy = reshape(qy, [im_height-pSize+1, im_width-pSize+1]);
+
+    % preenche borda repetindo elementos
+    qx = log(padarray(qx, [offset, offset], 'replicate'));
+    qy = log(padarray(qy, [offset, offset], 'replicate'));
+
+    qx(isnan(qx)) = min(min(qx(~isnan(qx))));
+    qy(isnan(qy)) = min(min(qy(~isnan(qy))));
+
+    q  = min(qx, qy);
+
+end
